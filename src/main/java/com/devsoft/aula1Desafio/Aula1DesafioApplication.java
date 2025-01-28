685ab6eb1523e939b5b9a5995750039848d99462
inacioads23 <jinacio11@gmail.com>
2025-01-27 22:33:54 -0300

    inject dependency

--- src/main/java/com/devsoft/aula1Desafio/Aula1DesafioApplication.java
+++ src/main/java/com/devsoft/aula1Desafio/Aula1DesafioApplication.java
@@ -1,21 +1,27 @@
 package com.devsoft.aula1Desafio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.devsoft.entities.Order;
+import com.devsoft.services.OrderService;
+
 @SpringBootApplication
 @ComponentScan({"com.devsoft"})
 public class Aula1DesafioApplication implements CommandLineRunner{
-
+	
+	@Autowired
+	private OrderService orderService;	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Aula1DesafioApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
 		
 	}
 

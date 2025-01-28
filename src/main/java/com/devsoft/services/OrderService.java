ec5cd03cef834d116878cc6075cf747db96e4945
inacioads23 <jinacio11@gmail.com>
2025-01-27 22:12:40 -0300

    implemented order value logic

--- src/main/java/com/devsoft/services/OrderService.java
+++ src/main/java/com/devsoft/services/OrderService.java
@@ -1,8 +1,20 @@
 package com.devsoft.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.devsoft.entities.Order;
 
 @Service
 public class OrderService {
 
+	@Autowired
+	private ShippingService shippingService;
+	
+	public double total(Order order) {		
+		double discount = order.getBasic() * (order.getDiscount() / 100);
+			
+		return order.getBasic() + shippingService.shipment(order) - discount;		
+	}
+	
 }
--- src/main/java/com/devsoft/services/ShippingService.java
+++ src/main/java/com/devsoft/services/ShippingService.java
@@ -2,7 +2,22 @@ package com.devsoft.services;
 
 import org.springframework.stereotype.Service;
 
+import com.devsoft.entities.Order;
+
 @Service
 public class ShippingService {
+	
+	public double shipment(Order order) {
+		double valor = order.getBasic();
+		double shipping = 0;
+		if(valor > 200.0){
+			shipping = 0.0;
+		}else if(valor >= 100.0){
+			shipping = 12.00;
+		}else if(valor < 100.0) {
+			shipping = 20.0;
+		}
+		return shipping;		
+	}
 
 }
